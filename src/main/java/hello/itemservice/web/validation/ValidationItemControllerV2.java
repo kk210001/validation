4d496f2553d5da950420ed6cf90cbd83e80965cd
kk210001 <kk210001@naver.com>
2022-09-06 22:08:03 +0900

    타임리프 스프링 검증 오류 기능 #fields 로 bindResult가 제공하는 검증 오류에 접근가능 th:error 필드에 오류가 있으면 이 태그 출력 th:if편의 버젼 th:errorclass th:field에서 필드에 오류가 있으면 class정보를 추가 해준다

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -47,7 +47,7 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
 
         //검증 로직
